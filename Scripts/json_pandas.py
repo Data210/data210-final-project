6b281977973e3a16c8b786617136cfe0e99e61a2
meghadeshmukh17 <megha.deshmukh@icloud.com>
2023-04-06 14:16:00 +0100

    changes to json_pandas.py

--- Scripts/json_pandas.py
+++ Scripts/json_pandas.py
@@ -16,32 +16,37 @@ json_df = []
 # Iterate over all JSON files in S3 bucket with given prefix
 response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
 
-'''
-The code uses s3.list_objects_v2 to retrieve a list of objects from the S3 bucket with the given prefix.
-The code then loops through the list of objects and checks if the key name ends with ".json".
-'''
+# The code uses s3.list_objects_v2 to retrieve a list of objects from the S3 bucket with the given prefix.
+# the code then loops through the list of objects and checks if the key name ends with ".json".
 for obj in response['Contents']:
     key = obj['Key']
     if key.endswith('.json'):
-        '''
-        Read JSON file from S3 into DataFrame
-        If the key ends with ".json", the code uses s3.get_object to read the contents of the file into a Python string. 
-        It then uses json.loads to convert the string into a Python dictionary, and 
-        pd.json_normalize to convert the dictionary into a Pandas DataFrame.
-        '''
         # Read JSON file from S3 into DataFrame
+        # If the key ends with ".json", the code uses s3.get_object to read the contents of the file into a Python string.
+        # It then uses json.loads to convert the string into a Python dictionary, and
+        # pd.json_normalize to convert the dictionary into a Pandas DataFrame.
         obj = s3.get_object(Bucket=bucket_name, Key=key)
         body = obj['Body'].read().decode('utf-8')
         data = json.loads(body)
-        df = pd.json_normalize(data)
+        try:
+            df = pd.json_normalize(data)
+        except ValueError:
+            print(f"Unable to normalize data from {key}")
+            continue
 
         # Append DataFrame to list
         json_df.append(df)
 
 # Concatenate all DataFrames into a single DataFrame
 df = pd.concat(json_df, ignore_index=True)
+# reset the index to create a new ID column
+df = df.reset_index()
+
+# rename the new ID column to 'id'
+df = df.rename(columns={'index': 'id'})
 
 print(df.head())
+
 #print(df.columns)
 
 # use explode function to split the list column into multiple rows
@@ -50,5 +55,5 @@ print(df_strengths.head())
 
 # use explode function to split the list column into multiple rows
 df_weaknesses = df[['id', 'weaknesses']].explode('weaknesses')
-print(df_weaknesses.head(15))
+print(df_weaknesses.head())
 
