01cc4994403a2febb8a372da79021a9dab9baa08
Harry Whitlock <harrywwhitlock@gmail.com>
2023-04-11 17:01:46 +0100

    Comments

--- Scripts/testscores2.py
+++ Scripts/testscores2.py
@@ -9,6 +9,7 @@ bucket = s3.Bucket('data-eng-210-final-project')
 
 df_all=pd.DataFrame()
 
+# Get all JSONs into a dataframe
 for obj in bucket.objects.all():
     if obj.key.endswith('.json'):
         x = obj.key
@@ -23,22 +24,32 @@ for obj in bucket.objects.all():
         
         df_all = pd.concat([df_all, df])
 
+# Read sparta_day_clean.csv
 df_sparta_day_clean = pd.read_csv('sparta_day_clean.csv')
 df_all['name'] = df_all['name'].str.upper()
 df_sparta_day_clean['name'] = df_sparta_day_clean['FirstName'] + ' ' + df_sparta_day_clean['LastName']
+
+# Merge the dataframes
 df_t = pd.merge(df_all, df_sparta_day_clean, on='name')
+
+# Extract tech self scores
 filtered_df = df_t.filter(regex='^tech_self_score|^SpartaDayTalentID$')
 filtered_df.columns = filtered_df.columns.str.replace("tech_self_score.", "")
+
+# Melt the dataframe
 df = filtered_df.melt(id_vars='SpartaDayTalentID', value_vars=filtered_df.columns[:-1], var_name='languages', value_name='scores')
 df = df.dropna()
 df = df.sort_values(by='SpartaDayTalentID')
 df['language_id'] = pd.factorize(df['languages'])[0]
 df=df.reset_index(drop=True)
+
+# Split into language table and self score table
 language_table = df[['languages', 'language_id']].drop_duplicates().reset_index(drop=True)
 tech_self_scores = df[['SpartaDayTalentID','language_id','scores']]
 tech_self_scores.rename(columns={"SpartaDayTalentID": "applicationid", "scores": "score"})
 language_table.rename(columns={"languages": "language"})
 
+# Write them into a csv
 with open('tech_self_scores.csv','w') as file:
     tech_self_scores.to_csv(file)
 
